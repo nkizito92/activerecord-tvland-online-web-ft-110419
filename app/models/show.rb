844c21697b67edb18b8694ad50a688aa0ac995be
nkizito92 <nkizito57@gmail.com>
2020-01-04 20:58:22 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,9 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
-  belongs_to :newtork
+  belongs_to :network
+
+  def actors_list
+    Actor.all.map {|act| act.first_name + " " + act.last_name}
+  end 
 end
